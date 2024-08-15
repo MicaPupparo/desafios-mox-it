dd986b7b18b0a9e139540bb0824eaf296d6e18be
MicaPupparo <micaelapupparo@gmail.com>
2024-08-15 16:25:03 -0300

    arregle ruta de la base de datos

--- flights/src/database/config/database.js
+++ flights/src/database/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: 'flights/src/database/database.sqlite'
+  storage: 'src/database/database.sqlite'
 });
 
 async function testConnection() {
